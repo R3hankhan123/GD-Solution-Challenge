42382ca4d121671918b09a374f45e314cc1bb8e8
Saksham Singh <sakshamsingh@Sakshams-MacBook-Air-2.local>
2023-03-01 14:40:30 +0530

    Created client and server folders, added client dependencies

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
